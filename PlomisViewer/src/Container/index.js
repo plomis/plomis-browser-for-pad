4284d0a587c3d80e62e8524248b55b3446fe8abb
jokemmy <252133226@qq.com>
2019-06-19 13:55:53 +0800

    去掉版本号主页显示

--- PlomisViewer/src/Container/index.js
+++ PlomisViewer/src/Container/index.js
@@ -1,38 +1,18 @@
 // @flow
 
 import React, { Component } from 'react';
-import codePush from "react-native-code-push";
-import { StyleSheet, View, Text, TouchableOpacity, Toast } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import ActionBar from '../ActionBar';
 import ViewPort from '../ViewPort';
 
 
 type Props = {};
 class Conainer extends Component<Props> {
-
-  handleCheckForUpdate = () => {
-    codePush.checkForUpdate()
-      .then(( update ) => {
-          if ( !update ) {
-            Toast.info( 'The app is up to date!' );
-          } else {
-            Toast.info( 'An update is available! Should we download it?' );
-          }
-      });
-  };
-
   render() {
     return (
       <ActionBar style={styles.container}>
         <View style={styles.viewPort}>
           <ViewPort />
-          <View style={styles.viewVersion}>
-            <TouchableOpacity onPress={this.handleCheckForUpdate}>
-              <Text>
-                1.0.16
-              </Text>
-            </TouchableOpacity>
-          </View>
         </View>
       </ActionBar>
     );
@@ -50,14 +30,6 @@ const styles = StyleSheet.create({
     flex: 1,
     zIndex: 1,
     position: 'relative'
-  },
-  viewVersion: {
-    position: 'absolute',
-    right: 20,
-    bottom: 20,
-    color: '#ffffff',
-    zIndex: 1,
-    backgroundColor: '#fff'
   }
 });
 
