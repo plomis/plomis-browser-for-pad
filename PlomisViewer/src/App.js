615fd1dee4f6afcba5b522a4ad085b9dcff949d1
jokemmy <252133226@qq.com>
2019-06-21 09:36:39 +0800

    更换图标，取消更新提示

--- PlomisViewer/src/App.js
+++ PlomisViewer/src/App.js
@@ -5,7 +5,7 @@ import codePush from "react-native-code-push";
 import SplashScreen from 'react-native-splash-screen';
 import Orientation from 'react-native-orientation-locker';
 import { createAppContainer, createStackNavigator } from 'react-navigation';
-import { StatusBar, StyleSheet, View, Alert } from 'react-native';
+import { StatusBar, StyleSheet, View } from 'react-native';
 import { Provider } from '@ant-design/react-native';
 import Container from './Container';
 
@@ -35,57 +35,57 @@ class App extends Component<PropsType> {
     this.syncImmediate();
   }
 
-  codePushStatusDidChange = ( syncStatus ) => {
-    switch( syncStatus ) {
-      case codePush.SyncStatus.UPDATE_INSTALLED:
-        this.installedAlert();
-        break;
-      case codePush.SyncStatus.UP_TO_DATE:
-        codePush.allowRestart();
-        break;
-      // case codePush.SyncStatus.CHECKING_FOR_UPDATE:
-      // case codePush.SyncStatus.DOWNLOADING_PACKAGE:
-      // case codePush.SyncStatus.AWAITING_USER_ACTION:
-      // case codePush.SyncStatus.INSTALLING_UPDATE:
-      // case codePush.SyncStatus.UPDATE_IGNORED:
-      case codePush.SyncStatus.UNKNOWN_ERROR:
-        this.errorAlert( '更新检查出错！' );
-        break;
-    }
-  };
+  // codePushStatusDidChange = ( syncStatus ) => {
+  //   switch( syncStatus ) {
+  //     case codePush.SyncStatus.UPDATE_INSTALLED:
+  //       this.installedAlert();
+  //       break;
+  //     case codePush.SyncStatus.UP_TO_DATE:
+  //       codePush.allowRestart();
+  //       break;
+  //     // case codePush.SyncStatus.CHECKING_FOR_UPDATE:
+  //     // case codePush.SyncStatus.DOWNLOADING_PACKAGE:
+  //     // case codePush.SyncStatus.AWAITING_USER_ACTION:
+  //     // case codePush.SyncStatus.INSTALLING_UPDATE:
+  //     // case codePush.SyncStatus.UPDATE_IGNORED:
+  //     // case codePush.SyncStatus.UNKNOWN_ERROR:
+  //     //   this.errorAlert( '更新检查出错！' );
+  //     //   break;
+  //   }
+  // };
 
-  errorAlert = ( message ) => {
-    Alert.alert(
-      '错误提示',
-      message,
-      [{
-        text: '知道了',
-        style: 'cancel'
-      }],
-      { cancelable: false }
-    );
-  };
+  // errorAlert = ( message ) => {
+  //   Alert.alert(
+  //     '错误提示',
+  //     message,
+  //     [{
+  //       text: '知道了',
+  //       style: 'cancel'
+  //     }],
+  //     { cancelable: false }
+  //   );
+  // };
 
-  installedAlert = () => {
-    Alert.alert(
-      '更新提示',
-      '更新已经下载完成，是否重启完成更新？',
-      [{
-        text: '以后',
-        style: 'cancel',
-        onPress() {
-          codePush.allowRestart();
-        }
-      }, {
-        text: '立即重启',
-        onPress() {
-          codePush.allowRestart();
-          codePush.restartApp();
-        }
-      }],
-      { cancelable: false }
-    );
-  };
+  // installedAlert = () => {
+  //   Alert.alert(
+  //     '更新提示',
+  //     '更新已经下载完成，是否重启完成更新？',
+  //     [{
+  //       text: '以后',
+  //       style: 'cancel',
+  //       onPress() {
+  //         codePush.allowRestart();
+  //       }
+  //     }, {
+  //       text: '立即重启',
+  //       onPress() {
+  //         codePush.allowRestart();
+  //         codePush.restartApp();
+  //       }
+  //     }],
+  //     { cancelable: false }
+  //   );
+  // };
 
   syncImmediate() {
     codePush.disallowRestart();
@@ -94,27 +94,27 @@ class App extends Component<PropsType> {
       // ON_NEXT_RESUME 下次恢复到前台时
       // ON_NEXT_RESTART 下一次重启时
       // IMMEDIATE 马上更新
-      installMode : codePush.InstallMode.ON_NEXT_RESTART,
-      //对话框
-      updateDialog : {
-        // 是否显示更新描述
-        appendReleaseDescription: false,
-        // 更新描述的前缀。 默认为"Description"
-        // descriptionPrefix: "更新内容：",
-        // 强制更新按钮文字，默认为continue
-        mandatoryContinueButtonLabel: "立即更新",
-        // 强制更新时的信息. 默认为"An update is available that must be installed."
-        mandatoryUpdateMessage: "必须更新后才能使用",
-        // 非强制更新时，按钮文字,默认为"ignore"
-        optionalIgnoreButtonLabel: '忽略更新',
-        // 非强制更新时，确认按钮文字. 默认为"Install"
-        optionalInstallButtonLabel: '下载更新',
-        // 非强制更新时，检查到更新的消息文本
-        optionalUpdateMessage: '有新版本了，是否更新？',
-        // Alert窗口的标题
-        title: '更新提示'
-      }
-    }, this.codePushStatusDidChange );
+      installMode : codePush.InstallMode.ON_NEXT_RESTART
+      // 对话框
+      // updateDialog : {
+      //   // 是否显示更新描述
+      //   appendReleaseDescription: false,
+      //   // 更新描述的前缀。 默认为"Description"
+      //   // descriptionPrefix: "更新内容：",
+      //   // 强制更新按钮文字，默认为continue
+      //   mandatoryContinueButtonLabel: "立即更新",
+      //   // 强制更新时的信息. 默认为"An update is available that must be installed."
+      //   mandatoryUpdateMessage: "必须更新后才能使用",
+      //   // 非强制更新时，按钮文字,默认为"ignore"
+      //   optionalIgnoreButtonLabel: '忽略更新',
+      //   // 非强制更新时，确认按钮文字. 默认为"Install"
+      //   optionalInstallButtonLabel: '下载更新',
+      //   // 非强制更新时，检查到更新的消息文本
+      //   optionalUpdateMessage: '有新版本了，是否更新？',
+      //   // Alert窗口的标题
+      //   title: '更新提示'
+      // }
+    });
   }
 
   render() {
